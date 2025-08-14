bc614da50477324ad3a2a2b08dc00b82a7ae1862
MrLYC <imyikong@gmail.com>
2025-08-14 15:57:27 +0800

    refactor: Remove file logging from PDF crawler configuration

--- src/pdf_helper/site_to_pdf.py
+++ src/pdf_helper/site_to_pdf.py
@@ -18,8 +18,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler("pdf_crawler.log")
+        logging.StreamHandler()
     ]
 )
 logger = logging.getLogger(__name__)
